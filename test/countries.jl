c5b6d729ca1c79a9440c248d1d78a4b55b02f794
Frank Herrmann <fgh@4gh.tv>
2014-11-06 13:18:50 +0000

    update tests for julia v0.4

--- test/countries.jl
+++ test/countries.jl
@@ -2,15 +2,17 @@ module TestWDICountries
 
 using Base.Test
 using WorldBankData
+using Compat
 
-country_data = { { "total"=>4,"per_page"=>"25000","pages"=>1,"page"=>1 },
-                 {
-                    ["latitude"=>"42.5075","id"=>"AND","iso2Code"=>"AD","incomeLevel"=>["id"=>"NOC","value"=>"High income: nonOECD"],"adminregion"=>["id"=>"","value"=>""],"lendingType"=>["id"=>"LNX","value"=>"Not classified"],"region"=>["id"=>"ECS","value"=>"Europe & Central Asia (all income levels)"],"capitalCity"=>"Andorra la Vella","name"=>"Andorra","longitude"=>"1.5218"],
-                    ["latitude"=>"","id"=>"ARB","iso2Code"=>"1A","incomeLevel"=>["id"=>"NA","value"=>"Aggregates"],"adminregion"=>["id"=>"","value"=>""],"lendingType"=>["id"=>"","value"=>"Aggregates"],"region"=>["id"=>"NA","value"=>"Aggregates"],"capitalCity"=>"","name"=>"Arab World","longitude"=>""],
-                    ["latitude"=>"24.4764","id"=>"ARE","iso2Code"=>"AE","incomeLevel"=>["id"=>"NOC","value"=>"High income: nonOECD"],"adminregion"=>["id"=>"","value"=>""],"lendingType"=>["id"=>"LNX","value"=>"Not classified"],"region"=>["id"=>"MEA","value"=>"Middle East & North Africa (all income levels)"],"capitalCity"=>"Abu Dhabi","name"=>"United Arab Emirates","longitude"=>"54.3705"],
-                    ["latitude"=>"-34.6118","id"=>"ARG","iso2Code"=>"AR","incomeLevel"=>["id"=>"UMC","value"=>"Upper middle income"],"adminregion"=>["id"=>"LAC","value"=>"Latin America & Caribbean (developing only)"],"lendingType"=>["id"=>"IBD","value"=>"IBRD"],"region"=>["id"=>"LCN","value"=>"Latin America & Caribbean (all income levels)"],"capitalCity"=>"Buenos Aires","name"=>"Argentina","longitude"=>"-58.4173"]
-                 }
-               }
+country_data = @compat Any[ Dict("total"=>4,"per_page"=>"25000","pages"=>1,"page"=>1),
+                 Any[
+                    Dict("latitude"=>"42.5075","id"=>"AND","iso2Code"=>"AD","incomeLevel"=>Dict("id"=>"NOC","value"=>"High income: nonOECD"),"adminregion"=>Dict("id"=>"","value"=>""),"lendingType"=>Dict("id"=>"LNX","value"=>"Not classified"),"region"=>Dict("id"=>"ECS","value"=>"Europe & Central Asia (all income levels)"),"capitalCity"=>"Andorra la Vella","name"=>"Andorra","longitude"=>"1.5218"),
+                    Dict("latitude"=>"","id"=>"ARB","iso2Code"=>"1A","incomeLevel"=>Dict("id"=>"NA","value"=>"Aggregates"),"adminregion"=>Dict("id"=>"","value"=>""),"lendingType"=>Dict("id"=>"","value"=>"Aggregates"),"region"=>Dict("id"=>"NA","value"=>"Aggregates"),"capitalCity"=>"","name"=>"Arab World","longitude"=>""),
+                    Dict("latitude"=>"24.4764","id"=>"ARE","iso2Code"=>"AE","incomeLevel"=>Dict("id"=>"NOC","value"=>"High income: nonOECD"),"adminregion"=>Dict("id"=>"","value"=>""),"lendingType"=>Dict("id"=>"LNX","value"=>"Not classified"),"region"=>Dict("id"=>"MEA","value"=>"Middle East & North Africa (all income levels)"),"capitalCity"=>"Abu Dhabi","name"=>"United Arab Emirates","longitude"=>"54.3705"),
+                    Dict("latitude"=>"-34.6118","id"=>"ARG","iso2Code"=>"AR","incomeLevel"=>Dict("id"=>"UMC","value"=>"Upper middle income"),"adminregion"=>Dict("id"=>"LAC","value"=>"Latin America & Caribbean (developing only)"),"lendingType"=>Dict("id"=>"IBD","value"=>"IBRD"),"region"=>Dict("id"=>"LCN","value"=>"Latin America & Caribbean (all income levels)"),"capitalCity"=>"Buenos Aires","name"=>"Argentina","longitude"=>"-58.4173"),
+                 ]
+               ]
+
 df_country = WorldBankData.parse_country(country_data)
 
 @test df_country[:name] == UTF8String["Andorra", "Arab World", "United Arab Emirates", "Argentina"]
--- test/wdi.jl
+++ test/wdi.jl
@@ -2,17 +2,20 @@ module TestParseWDI
 
 using Base.Test
 using WorldBankData
+using Compat
 
-us_gnp_data = {  { "total"=>23,"per_page"=>"25000","pages"=>1,"page"=>1 },
-                 { ["date"=>"2012","value"=>"52340","indicator"=>["id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"],"country"=>["id"=>"US","value"=>"United States"],"decimal"=>"0"],
-                   ["date"=>"2011","value"=>"50650","indicator"=>["id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"],"country"=>["id"=>"US","value"=>"United States"],"decimal"=>"0"],
-                   ["date"=>"2010","value"=>"48960","indicator"=>["id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"],"country"=>["id"=>"US","value"=>"United States"],"decimal"=>"0"],
-                   ["date"=>"2009","value"=>"48040","indicator"=>["id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"],"country"=>["id"=>"US","value"=>"United States"],"decimal"=>"0"],
-                   ["date"=>"2008","value"=>"49350","indicator"=>["id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"],"country"=>["id"=>"US","value"=>"United States"],"decimal"=>"0"],
-                   ["date"=>"2007","value"=>"48640","indicator"=>["id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"],"country"=>["id"=>"US","value"=>"United States"],"decimal"=>"0"],
-                   ["date"=>"2006","value"=>"48080","indicator"=>["id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"],"country"=>["id"=>"US","value"=>"United States"],"decimal"=>"0"]
-                 }
-              }
+
+us_gnp_data = @compat Any[  Dict( "total"=>23,"per_page"=>"25000","pages"=>1,"page"=>1 ),
+                 Any[ Dict("date"=>"2012","value"=>"52340","indicator"=>Dict("id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"),"country"=>Dict("id"=>"US","value"=>"United States"),"decimal"=>"0"),
+                   Dict("date"=>"2011","value"=>"50650","indicator"=>Dict("id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"),"country"=>Dict("id"=>"US","value"=>"United States"),"decimal"=>"0"),
+                   Dict("date"=>"2010","value"=>"48960","indicator"=>Dict("id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"),"country"=>Dict("id"=>"US","value"=>"United States"),"decimal"=>"0"),
+                   Dict("date"=>"2009","value"=>"48040","indicator"=>Dict("id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"),"country"=>Dict("id"=>"US","value"=>"United States"),"decimal"=>"0"),
+                   Dict("date"=>"2008","value"=>"49350","indicator"=>Dict("id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"),"country"=>Dict("id"=>"US","value"=>"United States"),"decimal"=>"0"),
+                   Dict("date"=>"2007","value"=>"48640","indicator"=>Dict("id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"),"country"=>Dict("id"=>"US","value"=>"United States"),"decimal"=>"0"),
+                   Dict("date"=>"2006","value"=>"48080","indicator"=>Dict("id"=>"NY.GNP.PCAP.CD","value"=>"GNI per capita, Atlas method (current US\$)"),"country"=>Dict("id"=>"US","value"=>"United States"),"decimal"=>"0")
+                 ]
+              ]
+
 
 us_gnp = WorldBankData.parse_wdi("NY.GNP.PCAP.CD",us_gnp_data[2],2006,2012)
 
