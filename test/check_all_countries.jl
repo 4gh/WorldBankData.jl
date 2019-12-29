7c6e370efb30d803c7d406bbf831a119791f4303
Frank Herrmann <fgh@4gh.tv>
2019-12-29 10:36:36 +0100

    autoformat 2 test files

--- test/check_all_countries.jl
+++ test/check_all_countries.jl
@@ -4,6 +4,6 @@ using WorldBankData
 @testset "check all countries" begin
 
     # checks the iso 2-letter country codes for updates
-    @test WorldBankData.download_countries(verbose=true)[!, :iso2c] == WorldBankData.all_countries
+    @test WorldBankData.download_countries(verbose = true)[!, :iso2c] == WorldBankData.all_countries
 
 end
