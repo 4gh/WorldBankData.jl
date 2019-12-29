7a30e33f83b6086950010580fc360018d09dcc7a
Frank Herrmann <fgh@4gh.tv>
2019-12-29 10:14:06 +0100

    move comment about iso2c country code test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,6 @@ include("indicators.jl")
 include("wdi.jl")
 include("search_wdi.jl")
 include("jsonwdi.jl")
-
-# checks the iso 2-letter country codes for updates
 include("check_all_countries.jl")
 
 # example_dl.jl downloads data from the world bank web site.
